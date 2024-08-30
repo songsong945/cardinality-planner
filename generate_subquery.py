0459b9de8e1f20bfcd937cd5ab89672cda13868b
songsong945 <songsong.mo@ntu.edu.sg>
2024-08-30 14:49:29 +0800

    Transferred revised function from version 9.6.6 to version 12.5

--- generate_subquery.py
+++ generate_subquery.py
@@ -1,75 +1,90 @@
-def table2sampleid(table_name):
-    t2sid = {
-        'title': 'tid',
-        'cast_info': 'ciid',
-        'movie_companies': 'mcid',
-        'movie_info_idx': 'miiid',
-        'movie_info': 'miid',
-        'movie_keyword': 'mkid'
-        }
-    return t2sid[table_name]
+import os
+import psycopg2
+import sqlparse
+from itertools import permutations
 
-def ali2table(ali):
-    a2t = {
-        't': 'title',
-        'ci': 'cast_info',
-        'mc': 'movie_companies',
-        'mi_idx': 'movie_info_idx',
-        'mi': 'movie_info',
-        'mk': 'movie_keyword',
-        'title': 'title',
-        'cast_info': 'cast_info',
-        'movie_companies': 'movie_companies',
-        'movie_info_idx': 'movie_info_idx',
-        'movie_info': 'movie_info',
-        'movie_keyword': 'movie_keyword'
-    }
-    return a2t[ali]
+def extract_tables_and_predicates(sql):
+    parsed = sqlparse.parse(sql)[0]
+    tables = {}
+    where_clause = ""
+    from_seen = False
+    where_seen = False
 
-csv_file = open('job-light-ranges_mm_wisdm_mscn.csv', 'r')
-#sql_file = open('job-light-ranges_mm_wisdm_deepdb.sql', 'r')
-csv_lines = csv_file.readlines()
-#sql_lines = sql_file.readlines()
-sub_dir = './mscn_subquery/'
-for i, line in enumerate(csv_lines):
-    outfile = open(sub_dir + 'q' + str(i) + '.csv', 'w')
-    tables = line.split('#')[0].split(',')
-    joins = line.split('#')[1].split(',')
-    predicates = line.split('#')[2].split(',')
-    result = line.split('#')[3]
-    num_tables = len(tables)
-    for k in range(2**num_tables):
-        csv_str = ''
-        tmp_tables = []
-        for l in range(num_tables):
-            if (k >> l) % 2 == 1:
-                tmp_tables.append(tables[l])
-        if len(tmp_tables) <  2:
-            continue
-        for t in tmp_tables:
-            csv_str += (t + ',')
-        csv_str = csv_str[:-1] + '#'
-        for j in joins:
-            t_c0 = j.split('=')[0]
-            t_c1 = j.split('=')[1]
-            t0 = t_c0.split('.')[0]
-            t1 = t_c1.split('.')[0]
-            if t0 in tmp_tables and t1 in tmp_tables:
-                csv_str += j + ','
-        if csv_str[-1] == ',':
-            csv_str = csv_str[:-1] +  '#'
-        else:
-            continue
-        for j in range(0, len(predicates), 3):
-            t_col = predicates[j]
-            op = predicates[j + 1]
-            val = predicates[j + 2]
-            t = t_col.split('.')[0] 
-            if t in tmp_tables:
-                csv_str += (t_col + ',' + op + ',' + val + ',')
-        if csv_str[-1] == ',':
-            csv_str = csv_str[:-1] + '#' + result 
-        else:
-            csv_str += ('#' + result)
-        outfile.write(csv_str)
-    outfile.close()
+    for token in parsed.tokens:
+        if from_seen and not where_seen:
+            if isinstance(token, sqlparse.sql.IdentifierList):
+                for identifier in token.get_identifiers():
+                    tables[identifier.get_real_name()] = ""
+            elif isinstance(token, sqlparse.sql.Identifier):
+                tables[token.get_real_name()] = ""
+        if where_seen:
+            where_clause += str(token)
+        if token.ttype is sqlparse.tokens.Keyword and token.value.upper() == "FROM":
+            from_seen = True
+        if token.ttype is sqlparse.tokens.Keyword and token.value.upper() == "WHERE":
+            where_seen = True
+    
+    for table in tables.keys():
+        if where_clause:
+            conditions = []
+            for condition in where_clause.split("AND"):
+                if table in condition:
+                    conditions.append(condition.strip())
+            tables[table] = " AND ".join(conditions)
+    
+    return tables
+
+def read_sql_files_from_directory(directory_path):
+    sql_files = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".sql"):
+            with open(os.path.join(directory_path, filename), 'r') as file:
+                sql_files.append(file.read())
+    return sql_files
+
+conn = psycopg2.connect(
+    dbname="imdb",
+    user="postgres",
+    password="",
+    host="localhost",
+    port="5432"
+)
+cursor = conn.cursor()
+
+sql_directory = 'path_to_your_sql_directory'
+sql_queries = read_sql_files_from_directory(sql_directory)
+
+with open('info.txt', 'w') as file:
+    for sql_query in sql_queries:
+        tables = extract_tables_and_predicates(sql_query)
+
+        results_cache = {}
+
+        for i in range(2, len(tables) + 1):
+            combos = permutations(tables.keys(), i)
+            for combo in combos:
+                combo_key = tuple(sorted(combo))  
+
+                if combo_key not in results_cache:
+                    from_clause = ', '.join(combo)
+                    where_clause = ' AND '.join([tables[alias] for alias in combo if tables[alias]])
+
+                    if where_clause:
+                        query = f"SELECT COUNT(*) FROM {from_clause} WHERE {where_clause}"
+                    else:
+                        query = f"SELECT COUNT(*) FROM {from_clause}"
+
+                    try:
+                        cursor.execute(query)
+                        count = cursor.fetchone()[0]
+                        results_cache[combo_key] = count
+                        print(f"Query successful for: {', '.join(combo)}, Count: {count}")
+                    except psycopg2.Error as e:
+                        print(f"Query failed for: {', '.join(combo)}. Error: {str(e)}")
+                        results_cache[combo_key] = None
+
+                count = results_cache.get(combo_key)
+                if count is not None:
+                    file.write(f"{', '.join(combo)}: {count}\n")
+
+conn.close()
